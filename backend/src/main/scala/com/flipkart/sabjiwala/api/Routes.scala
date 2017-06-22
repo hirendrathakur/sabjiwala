041d6c1f1c0ff35215e4d3268a5f8f741cd2998b
Mimani <saurabh.mimani@flipkart.com>
2017-06-22 20:36:39 +0530

    reading file and running tesseract command

--- backend/src/main/scala/com/flipkart/sabjiwala/api/Routes.scala
+++ backend/src/main/scala/com/flipkart/sabjiwala/api/Routes.scala
@@ -8,6 +8,8 @@ import com.flipkart.sabjiwala.directives.FileDirective
 import com.flipkart.sabjiwala.wire.{GenericResponse, JsonToEntityMarshaller, Response}
 
 import scala.util.{Failure, Success}
+import scala.sys.process._
+
 
 /**
   * Created by kinshuk.bairagi on 22/06/17.
@@ -28,7 +30,11 @@ class Routes extends FileDirective with JsonToEntityMarshaller {
           val fileInfo = postMap("file").right.get
           fileInfo.status match {
             case Success(_) =>
-              complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Upload Accepted: Tmp File Created", Map("tmpFile" -> fileInfo.tmpFilePath))))
+              val cmd = s"tesseract ${fileInfo.tmpFilePath} /tmp/out -l eng -c preserve_interword_spaces=1"
+              val output = cmd.!!
+              val source = scala.io.Source.fromFile("/tmp/out.txt")
+              val lines = try source.mkString finally source.close()
+              complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Upload Accepted: Tmp File Created", Map("tmpFile" -> fileInfo.tmpFilePath, "ouput" -> lines))))
             case Failure(e) =>
               //There was some isse processing the fileupload.
               println("Upload File Error", e)
