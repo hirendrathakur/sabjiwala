26ad998c1e339d83133b7d17920c08e81d745ccc
Mimani <saurabh.mimani@flipkart.com>
2017-06-23 03:15:36 +0530

    Making HTTL call to bigbasket

---
+++ backend/src/main/scala/com/flipkart/sabjiwala/services/CatalogService.scala
@@ -0,0 +1,59 @@
+package com.flipkart.sabjiwala.services
+
+import java.net.URLEncoder
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.{ActorMaterializer, Materializer}
+
+import scala.concurrent.duration._
+import com.flipkart.sabjiwala.utils.StringUtils._
+
+import scala.concurrent.{Await, Future}
+import HttpService.actorSystem
+import akka.http.scaladsl.model.headers.RawHeader
+import com.flipkart.sabjiwala.models.{BbData, BbProductServiceResponse}
+import com.flipkart.sabjiwala.utils.StringUtils._
+
+/**
+  * Created by saurabh.mimani on 22/06/17.
+  */
+object CatalogService {
+
+  implicit val materializer = ActorMaterializer()
+
+  def search(query: String)(implicit mat: Materializer): List[BbData] = {
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = s"https://www.bigbasket.com/productservice/autocomplete/?term=${URLEncoder.encode(query, "UTF-8")}&city_id=1", headers = Seq(RawHeader("Cookie", "_bb_vid=\"Mjk4NjkzOTA4Ng==\";")).asInstanceOf[scala.collection.immutable.Seq[HttpHeader]]))
+    val response = Await.result(responseFuture, 30.seconds)
+    val stringBody = response.entity.getString
+
+    val bbProductServiceResponse = stringBody.getObj[BbProductServiceResponse]
+    bbProductServiceResponse.results.data
+  }
+
+  def getDiscount(purchasedProducts: List[Map[String, Any]]): UploadResponse = {
+    println("purchasedProducts")
+    println(purchasedProducts)
+    var potentialSavings = 0.0
+    for(product <- purchasedProducts if product.nonEmpty && product("name").toString.nonEmpty) {
+      val result = search(product("name").asInstanceOf[String])
+      if(result.length > 0){
+        println(s"result for ${product("name").asInstanceOf[String]} and price ${product("price")}")
+        println(result)
+        val closest = result.minBy(v => math.abs(v.price.toDouble - product("price").toString.toDouble))
+        println(closest)
+        potentialSavings = potentialSavings + product("price").toString.toDouble - closest.price.toDouble
+      }
+    }
+    val uploadResponse = UploadResponse("BLR-WFLD-2017061098762", "10-June-2017", potentialSavings)
+    return uploadResponse
+  }
+
+  case class UploadResponse(
+                            invoiceNo: String,
+                            invoiceDate: String,
+                            totalSavings: Double
+                           )
+}
--- backend/src/main/scala/com/flipkart/sabjiwala/services/ParserService.scala
+++ backend/src/main/scala/com/flipkart/sabjiwala/services/ParserService.scala
@@ -11,29 +11,44 @@ import scala.util.matching.Regex
   */
 object ParserService {
 
-  def parse(file:String): List[String] ={
+  def parse(file:String): List[Map[String, Any]] ={
     val outFile = s"/tmp/read_${StringUtils.generateRandomStr(6)}"
     val cmd = s"tesseract $file $outFile -l eng -c preserve_interword_spaces=1"
     val output = cmd.!!
     val source = scala.io.Source.fromFile(s"$outFile.txt")
-    val lines = try source.getLines.toList.filter(_.contains("Rs")) finally source.close()
-    getFormatedResponse(lines)
+    var lines :List[String] = List()
+    var productLines :List[String] = List()
+    var invoiceLine :List[String] = List()
+    try {
+      lines = source.getLines.toList
+//      println("lines")
+//      println(lines)
+      productLines = lines.filter(_.contains("Rs"))
+
+      invoiceLine = lines.filter(_.contains("Invoice"))
+//      println("invoice lines")
+//      println(invoiceLine)
+
+    }
+    finally source.close()
+    getFormatedResponse(productLines)
   }
 
-  def getFormatedResponse(lines: List[String]): List[String] ={
+  def getFormatedResponse(lines: List[String]): List[Map[String, Any]] ={
     val regex1 = ".+?(?=Rs)(.*)".r
     val regex2 = ".+?(?=Rs)".r
     val regex3 = "([0-9]*[.]?[0-9]+)".r
     val formattedLines = lines.map { line =>
       val group1 = getGroup(regex2,0,line).toString.trim
       val group2 = getGroup(regex1,1,line)
-      val amount = getFirstMatch(regex3, group2)
+      val price = getFirstMatch(regex3, group2)
       val quantity =  getSecondMatch(regex3,group2)
-      var x = List()
-      regex3.findAllMatchIn(line).toList
-      List(group1, amount, quantity).mkString(",")
+//      List(group1, amount, quantity).mkString(",")
+      val r1 = "[A-Za-z]".r
+      val firstIdx = group1.indexOfSlice(r1.findFirstIn(group1).getOrElse(""))
+      Map("name" -> group1.substring(firstIdx), "price" -> price, "quantity" -> quantity)
     }
-    formattedLines
+    formattedLines.filter(_("name").nonEmpty)
   }
 
   def getGroup(regex:Regex, key:Int, line: String):String ={
